4e48030781957a849bbe055b7e014f768fbc5ee1
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-08-14 14:52:03 +0800

    Header and bread crumb to component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,23 +1,44 @@
-import { Button, ConfigProvider, Space } from 'antd';
+import { Layout, theme } from "antd";
+import { AntBreadcrumb } from "./component/AntBreadCrumb";
+import {AntHeader} from "./component/AntHeader";
+const { Content, Footer } = Layout;
 
-const App = () => (
-  <ConfigProvider
-    theme={{
-      token: {
-        // Seed Token
-        colorPrimary: '#00b96b',
-        borderRadius: 2,
-
-        // Alias Token
-        colorBgContainer: '#f6ffed',
-      },
-    }}
-  >
-    <Space>
-      <Button type="primary">Primary</Button>
-      <Button>Default</Button>
-    </Space>
-  </ConfigProvider>
-);
-
+const App = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout
+      style={{
+        margin: 0,
+      }}
+    >
+      <AntHeader />
+      <Content
+        style={{
+          padding: "0 48px",
+        }}
+      >
+        <AntBreadcrumb />
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: 280,
+            padding: 24,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©{new Date().getFullYear()} Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
 export default App;
