324b5bb7fe3b7152ee1a8ea40b6be13417a771da
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-08-14 13:06:20 +0800

    init
    include antd and daisyUI

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { Button, ConfigProvider, Space } from 'antd';
+
+const App = () => (
+  <ConfigProvider
+    theme={{
+      token: {
+        // Seed Token
+        colorPrimary: '#00b96b',
+        borderRadius: 2,
+
+        // Alias Token
+        colorBgContainer: '#f6ffed',
+      },
+    }}
+  >
+    <Space>
+      <Button type="primary">Primary</Button>
+      <Button>Default</Button>
+    </Space>
+  </ConfigProvider>
+);
+
+export default App;
