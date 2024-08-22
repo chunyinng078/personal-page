419f5ec578aa2a6a04859d64f810611d069bdb30
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-08-22 12:01:07 +0800

    Web Analytics

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,5 +1,6 @@
 // import { StrictMode } from 'react'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "./style.css";
