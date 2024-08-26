10475e4c85dc9ec5450f02a7cbacdbc5e16c5bb7
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2024-08-26 15:17:05 +0800

    Update App.tsx

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -12,7 +12,7 @@ const App = () => {
     <div className="">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={""} />
         </Routes>
         <Routes>
           <Route index element={<Home />} />
