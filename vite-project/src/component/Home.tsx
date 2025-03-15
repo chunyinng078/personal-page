6e4b7f49b982f605fa12daaf5b58f445030bf4ab
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2025-03-15 19:55:39 +0800

    color fix

--- vite-project/src/component/Home.tsx
+++ vite-project/src/component/Home.tsx
@@ -6,7 +6,7 @@ import BackToUp from "@uiw/react-back-to-top";
 import { Chat } from "./Chat";
 export const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-white font-sans text-lg text-black dark:bg-black dark:text-white">
+    <div className="flex flex-col items-center justify-center font-sans text-lg text-black dark:bg-black dark:text-white">
       <FlowBiteHeader />
       <div className="mt-4">
         <About />
