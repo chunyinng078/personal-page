33ef594e52de7c006bbc80dac955bd9970c8cc2c
NG CHUN YIN 22027058D <95467201+chunyinng078@users.noreply.github.com>
2026-03-10 18:37:09 +0800

    Update ProfileImage component with new image source

--- vite-project/src/component/ProfileImage.tsx
+++ vite-project/src/component/ProfileImage.tsx
@@ -2,7 +2,8 @@ import React from "react";
 const ProfileImage = (): JSX.Element => {
   return (
     <img
-      src="https://i.imgur.com/eyuaRXI.jpeg"
+      //src="https://i.imgur.com/eyuaRXI.jpeg"
+      src="https://media.licdn.com/dms/image/v2/D5603AQHiq1BTJPYU7Q/profile-displayphoto-crop_800_800/B56ZiUGbTHG0AI-/0/1754831370605?e=1774483200&v=beta&t=lXuownfRezdCwJThEYe8C7dZL3ZJnx-0YwhTogsNdK4"
       alt="profolio"
       className="sm:mb-5 rounded-full mx-auto w-64 h-64 object-cover"
       loading="lazy"
